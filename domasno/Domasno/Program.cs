fd7239da83f322e9e4e67c80b67c490dbfea789e
ivan02344 <48030924+ivan02344@users.noreply.github.com>
2019-03-07 19:04:28 +0100

    domasno

---
+++ domasno/Domasno/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domasno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum;
+            int secondNum;                   
+            string operation;
+            int answer;
+
+            Console.Write("Enter the first number: ");
+            firstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            secondNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter operation ( x , / , +, -) ");
+            operation = Console.ReadLine();
+
+            if (operation == "x")
+            {
+                answer = firstNum * secondNum;
+                Console.WriteLine(firstNum + " x " + secondNum + " = " + answer);
+                Console.ReadLine();
+            }
+            else if (operation == "/")
+            {
+                answer = firstNum / secondNum;
+                Console.WriteLine(firstNum + " / " + secondNum + " = " + answer);
+                Console.ReadLine();
+            }
+            else if (operation == "+")
+            {
+                answer = firstNum + secondNum;
+                Console.WriteLine(firstNum + " + " + secondNum + " = " + answer);
+                Console.ReadLine();
+            }
+            else if (operation == "-")
+            {
+                answer = firstNum - secondNum;
+                Console.WriteLine(firstNum + " - " + secondNum + " = " + answer);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Sorry that is not correct!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
